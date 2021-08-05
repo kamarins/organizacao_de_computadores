09c3df920eae37defbeef5d1386f38c399352dc6
kamarins <karineop@outlook.com>
2021-08-05 18:29:19 -0300

    fim

---
+++ trab2/define.h
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+#define tamanhoRam 1000
+#define tamanhoCache01 32
+#define tamanhoCache02 64
+#define tamanhoCache03 128
+#define tamanhoInstrucao 1000
+#define tamanhoPalavra 4
---
+++ trab2/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "mmu.h"
+#include "maquina.h"
+#include "blocodememoria.h"
+
+
+int main(){
+    
+    clock_t tempo;   // teste  
+
+    blocoMemoria **RAM = criaMemoriaRAM();
+    blocoMemoria **cache1 = criaMemoriaCache(tamanhoCache01);
+    blocoMemoria **cache2 = criaMemoriaCache(tamanhoCache02);
+    blocoMemoria **cache3 = criaMemoriaCache(tamanhoCache03);
+   
+
+    tempo = clock(); //aloca tempo inicial 
+
+    maquina(cache1, cache2, cache3, RAM);
+
+    tempo = clock() - tempo;
+    double *tempoPrograma = malloc (sizeof(double));
+    *tempoPrograma = ((double)tempo)/((CLOCKS_PER_SEC/1000));
+
+    printf("\nTempo de execucao: %.2lf ms.\n\n", *tempoPrograma);
+
+
+    liberaMemoria(cache1, cache2, cache3, RAM, tempoPrograma);
+    printf("DEU TUDO CERTO!\n");
+    return 0;
+}
