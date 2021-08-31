f0de0c59f2fd7a58b0be1c2ff9dc8c99cc9f7334
kamarins <karineop@outlook.com>
2021-08-31 20:16:15 -0300

    fim do 2 periodo

---
+++ tp_3/define.h
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+#define tamanhoCache01 8
+#define tamanhoCache02 16
+#define tamanhoCache03 32
+#define tamanhoRam 64  
+#define tamanhoHD 10000
+#define tamanhoInstrucao 1000
+#define tamanhoInterrupcao 30
+#define tamanhoPalavra 4
